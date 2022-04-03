cc058574f647142d307cb11faa568d1f9f7f23cb
Merlin <merlincro@hotmail.fr>
2022-04-03 22:56:24 +0200

    [UPD] Almost finished accueil

--- IDeA/src/app/accueil/accueil.component.ts
+++ IDeA/src/app/accueil/accueil.component.ts
@@ -11,38 +11,52 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 export class AccueilComponent implements OnInit {
 
   urlDax!: SafeUrl;
-  explorerClic!: boolean;
-  container!: string;
+  urlMarsan!: SafeUrl;
+  urlMimizan!: SafeUrl;
+  urlBouceau!: SafeUrl
+  urlCapbreton!: SafeUrl;
+  urlHossegor!: SafeUrl
+  isExplorer!: boolean;
+  content!: string;
+  accueil = [
+    {
+      status: "entreprise",
+      title: "Entreprise"
+    },
+    {
+      status: "collectivites",
+      title: "Collectivité"
+    },
+    {
+      status: "particuliers",
+      title: "Particulier"
+    }
+  ];
 
   constructor(public firebaseService: FirebaseService, public router: Router, private _sanitizer: DomSanitizer){}
 
   ngOnInit(): void {
     this.urlDax = this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/tZJlT948Qos');
-    this.explorerClic = false;
-    this.container = "container";
+    this.urlMarsan = this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/y18S3lp6Ox8');
+    this.urlMimizan = this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/Fxvc2bKtzeE');
+    this.urlBouceau = this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/NO5jRlh_N1k');
+    this.urlCapbreton = this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/vCmu42nJD8M');
+    this.urlHossegor = this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/fFNZFThijT4');
+    this.isExplorer = true;
+    this.content = "content";
   }
 
-  loginEntreprise(){
-    this.firebaseService.status = "entreprise";
+  login(status: string): void{
+    this.firebaseService.status = status;
     this.router.navigate(['login']);
   }
 
-  loginCollectivites(){
-    this.firebaseService.status = "collectivites";
-    this.router.navigate(['login']);
-  }
-
-  loginParticuliers(){
-    this.firebaseService.status = "particuliers";
-    this.router.navigate(['login']);
-  }
-
-  logout(){
+  logout(): void{
     this.firebaseService.logout();
   }
 
   onClickExplorer(): void {
-    this.explorerClic = true;
-    this.container = "container-explorer";
+    this.isExplorer = !this.isExplorer;
+    this.content = "content-blur";
   }
 }

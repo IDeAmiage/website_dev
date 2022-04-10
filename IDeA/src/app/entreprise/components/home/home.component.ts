5e2b4d1458a20cafed566f5943c7ebbe2e4f7de1
Junot <antoine.junotpro@gmail.com>
2022-04-10 14:17:18 +0200

    Adding style to home card + adding final style to sidenav.

--- IDeA/src/app/entreprise/components/home/home.component.ts
+++ IDeA/src/app/entreprise/components/home/home.component.ts
@@ -7,7 +7,7 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   /** Based on the screen size, switch from standard to one column per row */
@@ -15,29 +15,78 @@ export class HomeComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { imageSrc: "assets/Entreprise/foodtruck.jpg",title: 'Food-trucks', rlink:'foodtrucks',cols: 1, rows: 1 },
-          { imageSrc: "assets/Entreprise/covoiturage.jpg",title: 'Covoiturage', rlink:'covoiturage', cols: 1, rows: 1 },
-          { imageSrc: "assets/Entreprise/conferences.jpeg",title: 'Conférences', rlink:'conferences', cols: 1, rows: 1 },
-          { imageSrc: "assets/Entreprise/sport.jpg",title: 'Sport', rlink:'sport',cols: 1, rows: 1 }
+          {
+            imageSrc: 'assets/Entreprise/foodtruck.jpg',
+            title: 'Food-trucks',
+            rlink: 'foodtrucks',
+            cols: 1,
+            rows: 1,
+          },
+          {
+            imageSrc: 'assets/Entreprise/covoiturage.jpg',
+            title: 'Covoiturage',
+            rlink: 'covoiturage',
+            cols: 1,
+            rows: 1,
+          },
+          {
+            imageSrc: 'assets/Entreprise/conferences.jpeg',
+            title: 'Conférences',
+            rlink: 'conferences',
+            cols: 1,
+            rows: 1,
+          },
+          {
+            imageSrc: 'assets/Entreprise/sport.jpg',
+            title: 'Sport',
+            rlink: 'sport',
+            cols: 1,
+            rows: 1,
+          },
         ];
       }
 
       return [
-        { imageSrc: "assets/Entreprise/foodtruck.jpg",title: 'Food-trucks', rlink:'foodtrucks', cols: 1, rows: 1 },
-        { imageSrc: "assets/Entreprise/covoiturage.jpg",title: 'Covoiturage', rlink:'covoiturage', cols: 1, rows: 1 },
-        { imageSrc: "assets/Entreprise/conferences.jpeg",title: 'Conférences', rlink:'conferences', cols: 1, rows: 1 },
-        { imageSrc: "assets/Entreprise/sport.jpg",title: 'Sport', rlink:'sport', cols: 1, rows: 1 }
+        {
+          imageSrc: 'assets/Entreprise/foodtruck.jpg',
+          title: 'Food-trucks',
+          rlink: 'foodtrucks',
+          cols: 1,
+          rows: 1,
+        },
+        {
+          imageSrc: 'assets/Entreprise/covoiturage.jpg',
+          title: 'Covoiturage',
+          rlink: 'covoiturage',
+          cols: 1,
+          rows: 1,
+        },
+        {
+          imageSrc: 'assets/Entreprise/conferences.jpeg',
+          title: 'Conférences',
+          rlink: 'conferences',
+          cols: 1,
+          rows: 1,
+        },
+        {
+          imageSrc: 'assets/Entreprise/sport.jpg',
+          title: 'Sport',
+          rlink: 'sport',
+          cols: 1,
+          rows: 1,
+        },
       ];
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver, public firebase:FirebaseService, public router: Router) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    public firebase: FirebaseService,
+    public router: Router
+  ) {}
 
-  logout(){
+  logout() {
     this.firebase.logout();
     this.router.navigate(['/']);
   }
-
-
-
 }

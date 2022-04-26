4107673978206717dd28d4d9f13938754bb27189
A.J <antoine.junotpro@gmail.com>
2022-04-26 21:26:18 +0200

    [UPD]Adding card style + responsive card on Home page. Have to fixed 'View button'

--- IDeA/src/app/entreprise/components/home/home.component.ts
+++ IDeA/src/app/entreprise/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Router } from '@angular/router';
 import { FirebaseService } from './../../../firebase.service';
-import { Component, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 
@@ -9,7 +9,17 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  cols : number | undefined;
+
+  gridByBreakpoint = {
+    xl: 4,
+    lg: 4,
+    md: 2,
+    sm: 2,
+    xs: 1
+  }
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
@@ -79,14 +89,45 @@ export class HomeComponent {
     })
   );
 
+
   constructor(
-    private breakpointObserver: BreakpointObserver,
     public firebase: FirebaseService,
-    public router: Router
-  ) {}
+    public router: Router,
+    private breakpointObserver: BreakpointObserver) {
+      this.breakpointObserver.observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge,
+      ]).subscribe(result => {
+        if (result.matches) {
+          if (result.breakpoints[Breakpoints.XSmall]) {
+            this.cols = this.gridByBreakpoint.xs;
+          }
+          if (result.breakpoints[Breakpoints.Small]) {
+            this.cols = this.gridByBreakpoint.sm;
+          }
+          if (result.breakpoints[Breakpoints.Medium]) {
+            this.cols = this.gridByBreakpoint.md;
+          }
+          if (result.breakpoints[Breakpoints.Large]) {
+            this.cols = this.gridByBreakpoint.lg;
+          }
+          if (result.breakpoints[Breakpoints.XLarge]) {
+            this.cols = this.gridByBreakpoint.xl;
+          }
+        }
+      });
+    }
+
+    ngOnInit() {
+    }
 
   logout() {
     this.firebase.logout();
     this.router.navigate(['/']);
   }
+
+
 }

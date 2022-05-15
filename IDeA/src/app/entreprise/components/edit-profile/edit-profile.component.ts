488050d8f71685798506308dcc454ec3a75b67c2
Merlin <merlincro@hotmail.fr>
2022-05-15 23:35:58 +0200

    [UPD] Fixed edit profile MC

--- IDeA/src/app/entreprise/components/edit-profile/edit-profile.component.ts
+++ IDeA/src/app/entreprise/components/edit-profile/edit-profile.component.ts
@@ -2,9 +2,10 @@ import { NavComponent } from './../nav/nav.component';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FirestorageService } from 'src/app/firestorage.service';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
 import { KeyValue } from '@angular/common';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ProfileComponent } from '../profile/profile.component';
 
 @Component({
   selector: 'app-edit-profile',
@@ -15,35 +16,37 @@ export class EditProfileComponent implements OnInit {
 
   currentUser : any;
   editable : any = ["_name","_phone","_car", "_strava_account","_entreprise"]
-  editActive = false;
   fuelList: string[] = ["diesel", "essence", "gpl", "cng"];
   sizeList: string[] = ['Little', 'Medium', 'Big'];
+  capacity: number[] = [1,2,3,4];
   userForms: FormGroup;
 
   constructor(public firestore: FirestorageService, public router: Router,
-    public dialogRef: MatDialogRef<NavComponent>) {
+    public dialogRef: MatDialogRef<NavComponent>, public dialog: MatDialog) {
       let defaultInputText = {value: '', disabled: true};
-    let defaultInputNumber = {value: 0, disabled: true};
-    let followInput = {value: '', disabled: !this.editActive};
+      let defaultInputNumber = {value: 0, disabled: true};
+      let followInputText = {value: '', disabled: false};
+      let followInputNumber = {value: 0, disabled: false};
 
     this.userForms = new FormGroup({
-      _carburant: new FormControl(followInput),
-      _type: new FormControl(followInput),
+      _carburant: new FormControl(followInputText, [Validators.required]),
+      _type: new FormControl(followInputText, [Validators.required]),
       _id: new FormControl(defaultInputText),
       _score: new FormControl(defaultInputNumber),
       _avis: new FormControl(defaultInputText),
       _note: new FormControl(defaultInputNumber),
       _entreprise: new FormControl(defaultInputText),
-      _name: new FormControl(followInput),
-      _phone: new FormControl(followInput, [Validators.required, Validators.pattern(/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/gmi)]),
+      _name: new FormControl(followInputText, [Validators.required]),
+      _phone: new FormControl(followInputText, [Validators.required, Validators.pattern(/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/gmi)]),
       _strava_account: new FormControl(defaultInputText),
       _level: new FormControl(defaultInputNumber),
       _nbTrajects: new FormControl(defaultInputNumber),
       _co2: new FormControl(defaultInputNumber),
+      _capacite: new FormControl(followInputNumber, [Validators.required]),
     });
     }
 
-  ngOnInit() {
+  public ngOnInit() {
     this.firestore.getUser(localStorage.getItem('user_id')!).subscribe(res=>{
      this.currentUser = res[0];
      this.userForms.patchValue(this.currentUser._car);
@@ -51,11 +54,23 @@ export class EditProfileComponent implements OnInit {
     });
   }
 
-  save(){
-    console.log(this.currentUser);
-
+  public onSubmit(){
+    this.currentUser._name = this.userForms.controls['_name'].value;
+    this.currentUser._phone = this.userForms.controls['_phone'].value;
+    this.currentUser._car._capacite = this.userForms.controls['_capacite'].value;
+    this.currentUser._car._type = this.userForms.controls['_type'].value;
+    this.currentUser._car._carburant = this.userForms.controls['_carburant'].value;
     this.firestore.updateUser(this.currentUser, this.currentUser._id);
-    this.dialogRef.close();
+    this.openProfile();
   }
 
+  public openProfile(){
+    this.dialogRef.close();
+    const dialogConfig = new MatDialogConfig();
+    // dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "25%";
+    //dialogConfig.height = "60%";
+    this.dialog.open(ProfileComponent, dialogConfig);
+  }
 }

9d250944016ce8283fa04052e845ca3835d096c0
John Doe <merlincro@hotmail.fr>
2022-05-15 13:47:17 +0200

    [UPD] Resolved merge problems

--- IDeA/src/app/entreprise/components/covoiturage/covoiturage.component.ts
+++ IDeA/src/app/entreprise/components/covoiturage/covoiturage.component.ts
@@ -1,5 +1,3 @@
-import { OpendatasoftV1Service } from './../../../opendatasoftV1.service';
-import { NotifierService } from './../../../notifier.service';
 import { LoaderService } from './../../../loader.service';
 import { Trajet } from './../../../Trajet';
 import { PostCovoiturageComponent } from './../post-covoiturage/post-covoiturage.component';
@@ -38,9 +36,6 @@ export class CovoiturageComponent implements OnInit {
   constructor(public firebase: FirebaseService, public router: Router,
      public firestore: FirestorageService, public dialog: MatDialog,
      public loader: LoaderService,
-     public notifier: NotifierService,
-     public opendatasoft: OpendatasoftV1Service) { }
-
      private breakpointObserver: BreakpointObserver) {
       this.breakpointObserver.observe([
         Breakpoints.XSmall,
@@ -67,18 +62,24 @@ export class CovoiturageComponent implements OnInit {
           }
         }
       });
-  }
+    }
 
   data = {name:'test'};
 
   ngOnInit(): void {
     this.loadTrajects();
+    console.log(parseFloat(localStorage.getItem('latitude')!));
+    console.log(parseFloat(localStorage.getItem('longitude')!));
   }
 
   logout(){
     this.firebase.logout();
     this.router.navigate(['/']);
   }
+
+  // testfirestore(){
+  //   this.firestore.insertObject(this.data,"test");
+  // }
 
   loadTrajects(){
     const obj = this.firestore.getObject("trajet");
@@ -97,16 +98,19 @@ export class CovoiturageComponent implements OnInit {
                 longitude: parseFloat(element._start_longitude),
             })/1000
         )
+
       })
       this.TrajetListe.sort((a:any, b:any) =>
         this.Userdistances[this.TrajetListe.indexOf(a)] - this.Userdistances[this.TrajetListe.indexOf(b)]
       );
       this.Userdistances.sort((a:any, b:any) => a - b);
     })
+
   }
 
   onCreate(){
     const dialogConfig = new MatDialogConfig();
+    // dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
     dialogConfig.width = "60%";
     this.dialog.open(PostCovoiturageComponent, dialogConfig);
@@ -126,22 +130,4 @@ export class CovoiturageComponent implements OnInit {
     this.firestore.deleteTraject(this.TrajetListe[i], localStorage.getItem('user_id')!);
     this.isChecked = false;
   }
-
-  addPassenger(i:number){
-    this.firestore.getUser(localStorage.getItem('user_id')!).subscribe(res=>{
-      localStorage.setItem('user',JSON.stringify(res));
-      const user = JSON.parse(localStorage.getItem("user")!);
-      if (this.TrajetListe[i]._passagers.length == this.TrajetListe[i]._user._car._capacite){
-        this.notifier.showNotification("This traject is full","OK", "error");
-      } else if (this.TrajetListe[i]._passagers.some((item:any) => item._id === user[0]._id)){
-        this.notifier.showNotification("You are already register","OK","error");
-      }
-      this.TrajetListe[i]._passagers.push(user[0]);
-      this.firestore.updateTraject(this.TrajetListe[i]);
-    })
-  }
-
-
-
-
 }

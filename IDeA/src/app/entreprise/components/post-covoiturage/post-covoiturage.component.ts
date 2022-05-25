c9473222fe1fe85990a7f1e38d7052a54227a43f
kadiatou <kadiatousidibe679@gmail.com>
2022-05-25 11:18:07 +0200

    Ajout de messages d'erreur sur la publication d'annonce

--- IDeA/src/app/entreprise/components/post-covoiturage/post-covoiturage.component.ts
+++ IDeA/src/app/entreprise/components/post-covoiturage/post-covoiturage.component.ts
@@ -24,19 +24,23 @@ export class PostCovoiturageComponent implements OnInit {
   arrivee = new Adress();
 
   trajet = new Trajet();
+  isCarFilled = false;
+  isTrajetFilled = false;
+  isDateTimeFilled = false;
+  isRecurrenceFilled = false;
 
   departure_date = new Date();
-  departure_time:any;
+  departure_time: any;
 
-  currentUser:any;
-  freq: string="";
+  currentUser: any;
+  freq: string = "";
 
   panelOpenState = false;
   size = new FormControl();
   fuel = new FormControl();
   people = new FormControl();
   sizeList: string[] = ['Little', 'Medium', 'Big'];
-  capacity: number[] = [1,2,3,4];
+  capacity: number[] = [1, 2, 3, 4];
 
   step = 0;
 
@@ -52,44 +56,64 @@ export class PostCovoiturageComponent implements OnInit {
     this.step--;
   }
 
-  fuelList: string[] = ["diesel","essence","gpl","cng"]
+  fuelList: string[] = ["diesel", "essence", "gpl", "cng"]
 
-  constructor(public firebase:FirebaseService, public api: ApiService, public firestore: FirestorageService,
+  constructor(public firebase: FirebaseService, public api: ApiService, public firestore: FirestorageService,
     public dialogRef: MatDialogRef<CovoiturageComponent>, public router: Router, private notifier: NotifierService
-    ) {
+  ) {
   }
 
   ngOnInit() {
-    this.firestore.getUser(localStorage.getItem('user_id')!).subscribe(res=>{
+    this.firestore.getUser(localStorage.getItem('user_id')!).subscribe(res => {
       this.currentUser = res;
       console.log(this.currentUser[0]);
     })
 
   }
 
-  saveCar(){
-    this.userCar._carburant = this.fuel.value;
-    this.userCar._type = this.size.value;
-    this.userCar._capacite = this.people.value;
-    this.trajet._user = this.currentUser[0];
-    this.trajet._user._car = this.userCar;
-    this.nextStep();
+  saveCar() {
+    if (this.fuel.value && this.size.value && this.people.value) {
+      this.userCar._carburant = this.fuel.value;
+      this.userCar._type = this.size.value;
+      this.userCar._capacite = this.people.value;
+      this.trajet._user = this.currentUser[0];
+      this.trajet._user._car = this.userCar;
+      this.isCarFilled = true;
+      this.nextStep();
+    } else {
+      this.notifier.showNotification("Une erreur s'est produite, merci de remplir tous les champs", "OK", "error");
+    }
+
   }
 
-  saveDateTime(){
-    this.trajet._departure_time = new Date(
-     this.departure_date.getFullYear(),
-     this.departure_date.getMonth(),
-     this.departure_date.getDate(),
-     parseInt(this.departure_time.split(':')[0]),
-     parseInt(this.departure_time.split(':')[1])
-    );
-    this.nextStep();
-    
+  saveDateTime() {
+    if (this.departure_time) {
+      this.trajet._departure_time = new Date(
+        this.departure_date.getFullYear(),
+        this.departure_date.getMonth(),
+        this.departure_date.getDate(),
+        parseInt(this.departure_time.split(':')[0]),
+        parseInt(this.departure_time.split(':')[1])
+      );
+      this.isDateTimeFilled = true;
+      this.nextStep();
+    }
+    else {
+      this.notifier.showNotification("Une erreur s'est produite, merci de remplir tous les champs", "OK", "error");
+    }
+
+
   }
 
-  saveRecurrent(){
-    console.log(this.freq);
+  saveRecurrent() {
+    if (this.freq) {
+      console.log(this.freq);
+      this.isRecurrenceFilled = true;
+      this.nextStep();
+    } else {
+      this.notifier.showNotification("Une erreur s'est produite, merci de remplir tous les champs", "OK", "error");
+    }
+
 
     // switch (this.freq) {
     //   case "day":
@@ -115,19 +139,25 @@ export class PostCovoiturageComponent implements OnInit {
     //   default:
     //     break;
     // }
-    this.nextStep();
+
   }
 
-  async getLatLong(adresse: Adress){
+  async getLatLong(adresse: Adress) {
 
-    await this.api.getPositionFromAdress(adresse._num, adresse._addresse, adresse._city).toPromise().then(response=>{
-      const obj = JSON.parse(JSON.stringify(response));
-      adresse._lat = obj["0"].lat;
-      adresse._lon = obj["0"].lon;
-    })
+    if (adresse._num && adresse._addresse, adresse._city) {
+      await this.api.getPositionFromAdress(adresse._num, adresse._addresse, adresse._city).toPromise().then(response => {
+        const obj = JSON.parse(JSON.stringify(response));
+        adresse._lat = obj["0"].lat;
+        adresse._lon = obj["0"].lon;
+      })
+      this.isTrajetFilled = true;
+    } else {
+      this.notifier.showNotification("Une erreur s'est produite, merci de remplir tous les champs", "OK", "error");
+    }
+
   }
 
-  async calculateDistance(ad1: Adress, ad2: Adress){
+  async calculateDistance(ad1: Adress, ad2: Adress) {
     await this.getLatLong(ad1);
     await this.getLatLong(ad2);
     var dist = geolib.getDistance(
@@ -142,66 +172,74 @@ export class PostCovoiturageComponent implements OnInit {
 
 
 
-  async saveTraject(){
+  async saveTraject() {
     await this.calculateDistance(this.depart, this.arrivee);
-    this.trajet._user._car = Object.assign({}, this.trajet._user._car)
-    console.log(this.trajet._user._car);
-    this.trajet._user = Object.assign({}, this.trajet._user)
-    this.trajet._user._nbTrajects++;
-    this.trajet._user._co2 = this.trajet._user._co2 + this.trajet._co2Emission;
-    this.trajet = Object.assign({}, this.trajet)
-    await this.api.getCo2Calculation(this.trajet).toPromise().then((res)=>{
-      this.trajet._co2Emission = parseFloat(res.toString());
-    })
-    console.log(this.freq);
+    console.log("1", this.isCarFilled, '2', this.isDateTimeFilled, "3", this.isRecurrenceFilled, "4" , this.isTrajetFilled);
+    if (!this.isCarFilled || !this.isDateTimeFilled || !this.isRecurrenceFilled || !this.isTrajetFilled) {
+      this.notifier.showNotification("Une erreur s'est produite, merci de remplir tous les champs", "OK", "error");
+    } else {
 
-    switch (this.freq) {
-      case "day":
-        this.firestore.insertObject(this.trajet,"trajet");
-        for (let i = 1; i < 7; i++) {
-          this.trajet._departure_time = new Date(this.trajet._departure_time.setDate(this.trajet._departure_time.getDate() +1));
-          this.firestore.insertObject(this.trajet,"trajet");
-        }
-        this.firestore.updateUser(this.trajet._user, this.trajet._user._id).then((item:any)=>{
-          location.reload();
-          this.dialogRef.close();
-        })
-        break;
-      case "week":
-        this.firestore.insertObject(this.trajet,"trajet");
-        for (let i = 1; i < 8; i++) {
-          this.trajet._departure_time = new Date(this.trajet._departure_time.setDate(this.trajet._departure_time.getDate() +7*1));
-          this.firestore.insertObject(this.trajet,"trajet");
-        }
-        this.firestore.updateUser(this.trajet._user, this.trajet._user._id).then((item:any)=>{
-          location.reload();
-          this.dialogRef.close();
-        })
-        break;
-      case "month":
-        this.firestore.insertObject(this.trajet,"trajet");
-        for (let i = 1; i < 12; i++) {
-          this.trajet._departure_time = new Date(this.trajet._departure_time.setDate(this.trajet._departure_time.getMonth() +1));
-          this.firestore.insertObject(this.trajet,"trajet");
-        }
-        this.firestore.updateUser(this.trajet._user, this.trajet._user._id).then((item:any)=>{
-          location.reload();
-          this.dialogRef.close();
-        })
-        break;
-      case "aucun":
-        this.firestore.insertObject(this.trajet,"trajet");
-        this.firestore.updateUser(this.trajet._user, this.trajet._user._id).then((item:any)=>{
-          location.reload();
-          this.dialogRef.close();
-        })
-        break;
-      default:
-        break;
+      
+      this.trajet._user._car = Object.assign({}, this.trajet._user._car)
+      console.log(this.trajet._user._car);
+      this.trajet._user = Object.assign({}, this.trajet._user)
+      this.trajet._user._nbTrajects++;
+      this.trajet._user._co2 = this.trajet._user._co2 + this.trajet._co2Emission;
+      this.trajet = Object.assign({}, this.trajet)
+      await this.api.getCo2Calculation(this.trajet).toPromise().then((res) => {
+        this.trajet._co2Emission = parseFloat(res.toString());
+      })
+      console.log(this.freq);
+
+      switch (this.freq) {
+        case "day":
+          this.firestore.insertObject(this.trajet, "trajet");
+          for (let i = 1; i < 7; i++) {
+            this.trajet._departure_time = new Date(this.trajet._departure_time.setDate(this.trajet._departure_time.getDate() + 1));
+            this.firestore.insertObject(this.trajet, "trajet");
+          }
+          this.firestore.updateUser(this.trajet._user, this.trajet._user._id).then((item: any) => {
+            location.reload();
+            this.dialogRef.close();
+          })
+          break;
+        case "week":
+          this.firestore.insertObject(this.trajet, "trajet");
+          for (let i = 1; i < 8; i++) {
+            this.trajet._departure_time = new Date(this.trajet._departure_time.setDate(this.trajet._departure_time.getDate() + 7 * 1));
+            this.firestore.insertObject(this.trajet, "trajet");
+          }
+          this.firestore.updateUser(this.trajet._user, this.trajet._user._id).then((item: any) => {
+            location.reload();
+            this.dialogRef.close();
+          })
+          break;
+        case "month":
+          this.firestore.insertObject(this.trajet, "trajet");
+          for (let i = 1; i < 12; i++) {
+            this.trajet._departure_time = new Date(this.trajet._departure_time.setDate(this.trajet._departure_time.getMonth() + 1));
+            this.firestore.insertObject(this.trajet, "trajet");
+          }
+          this.firestore.updateUser(this.trajet._user, this.trajet._user._id).then((item: any) => {
+            location.reload();
+            this.dialogRef.close();
+          })
+          break;
+        case "aucun":
+          this.firestore.insertObject(this.trajet, "trajet");
+          this.firestore.updateUser(this.trajet._user, this.trajet._user._id).then((item: any) => {
+            location.reload();
+            this.dialogRef.close();
+          })
+          break;
+        default:
+          break;
+      }
     }
+
   }
 }
 
 function delay(ms: number) {
-  return new Promise( resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
